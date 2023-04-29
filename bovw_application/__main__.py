0d9b3bc2fc2de9d478d81b861e7b12636c0b30a7
jfgagnon_00 <jfgagnon00_00@yahoo.com>
2023-04-29 19:20:32 -0400

    Code minimal pour faire classification. Manque UI.

--- bovw_application/__main__.py
+++ bovw_application/__main__.py
@@ -7,3 +7,14 @@ from helpers.visual_words.VisualWords import VisualWords
 if __name__ == "__main__":
     configs = hlp.get_configs("config_overrides.json")
     visual_words = VisualWords.from_sift_configs(configs)
+
+    # image = cv2.imread("dataset/demo/Grape___healthy/image (1).JPG")
+    image = cv2.imread("dataset/demo/Grape___Esca_(Black_Measles)/image (45).JPG")
+
+    key_points, features, tf_idf, label = visual_words.predict(image)
+
+    print("Image shape:", image.shape)
+    print("Key points:", len(key_points))
+    print("Features shape:", features.shape)
+    print("TF-IDF shape:", tf_idf.shape)
+    print("Label:", label)
