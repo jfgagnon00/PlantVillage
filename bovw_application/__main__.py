1c481eea0712a7ea0fb5963380a57e864e51ed1c
jfgagnon_00 <jfgagnon00_00@yahoo.com>
2023-04-29 19:31:36 -0400

    Gestion minimale d'erreur

--- bovw_application/__main__.py
+++ bovw_application/__main__.py
@@ -5,16 +5,25 @@ from helpers.visual_words.VisualWords import VisualWords
 
 
 if __name__ == "__main__":
+    # relire le modele pre-entraine
     configs = hlp.get_configs("config_overrides.json")
     visual_words = VisualWords.from_sift_configs(configs)
 
+    # obtenir une image
     # image = cv2.imread("dataset/demo/Grape___healthy/image (1).JPG")
-    image = cv2.imread("dataset/demo/Grape___Esca_(Black_Measles)/image (45).JPG")
+    # image = cv2.imread("dataset/demo/Grape___Esca_(Black_Measles)/image (45).JPG")
+    image = cv2.imread("dataset/demo/astronaut.jpg")
 
-    key_points, features, tf_idf, label = visual_words.predict(image)
+    if not image is None:
+        # s'assurer que l'image est de la meme taille que celles de lors de l'entrainement
+        image = cv2.resize(image, dsize=(256, 256), interpolation=cv2.INTER_LINEAR)
 
-    print("Image shape:", image.shape)
-    print("Key points:", len(key_points))
-    print("Features shape:", features.shape)
-    print("TF-IDF shape:", tf_idf.shape)
-    print("Label:", label)
+        # faire prediciton
+        key_points, features, tf_idf, label = visual_words.predict(image)
+
+        # afficher les resultats
+        print("Image shape:", image.shape)
+        print("Key points:", len(key_points))
+        print("Features shape:", features.shape)
+        print("TF-IDF shape:", tf_idf.shape)
+        print("Label:", label)
